664a2fbd04c891f30e56cef91c35e72a9d03706f
pavlo <volochaypavlo247@gmail.com>
2024-09-24 21:18:47 +0300

    init

---
+++ src/main/java/com/chmnu_ki_123/k3/MyFirstJavaProgram.java
@@ -0,0 +1,14 @@
+package com.chmnu_ki_123.k3;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class MyFirstJavaProgram {
+
+    /* This is my first java program.
+     * This will print 'Hello World' as the output
+     */
+
+    public static void main(String []args) {
+        System.out.println("Hello World"); // prints Hello World
+    }
+}
